988da6c551f612829e81d86d1df80d8164a3176c
Rezan Labady <rezan.labady@gmail.com>
2018-09-20 16:37:19 +0200

    hejhej

---
+++ Bootstrap/wwwroot/Bootstrap.js
@@ -0,0 +1,27 @@
+﻿let home = document.getElementById("home")
+
+home.addEventListener("click", () => {
+
+	document.location.href = 'https://localhost:44333/Main.html'
+
+
+});
+
+let shop = document.getElementById("shop")
+
+shop.addEventListener("click", () => {
+
+document.location.href = 'https://localhost:44333/shop.html'
+
+
+});
+
+let contact = document.getElementById("contact")
+
+contact.addEventListener("click", () => {
+
+
+	document.location.href = 'https://localhost:44333/Contact.html'
+
+
+});
